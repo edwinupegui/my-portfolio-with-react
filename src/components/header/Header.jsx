15a65a18e940d7b26fdcd11d0a96580b7c879a6c
Edwin Upegui <edwinupegui@gmail.com>
2022-05-15 20:26:21 -0500

    fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials'
-import About from '../about/About'
 
 const Header = () => {
   return (
